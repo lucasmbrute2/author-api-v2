598625d7bb2159a2d939845a05a0b38d06bee4f1
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-19 14:28:21 -0300

    feat: create author repository

---
+++ src/application/repositories/authors-repositories.ts
@@ -0,0 +1,6 @@
+import { Author } from '../modules/author/entities/author'
+
+export interface AuthorsRepository {
+  create(author: Author): Promise<void>
+  findByUsername(username: string): Promise<Author | null>
+}
