c161d8b12e29d123f77188787fc004e8029dbd41
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-19 18:25:59 -0300

    feat: create change password use case specs

---
+++ src/application/modules/author/use-cases/change-password/change-password.spec.ts
@@ -0,0 +1,45 @@
+import { InMemoryAuthorsRepository } from '@/application/repositories/in-memory/in-memory-authors-repository'
+import { compare, hash } from 'bcryptjs'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { makeAuthor } from '../../factory/make-author'
+import { InvalidCredentialsError } from '../errors/invalid-credentials-error'
+import { ChangePasswordUseCase } from './change-password'
+
+let sut: ChangePasswordUseCase
+let inMemoryAuthorsRepository: InMemoryAuthorsRepository
+
+describe('Change password use case', () => {
+  beforeEach(() => {
+    inMemoryAuthorsRepository = new InMemoryAuthorsRepository()
+    sut = new ChangePasswordUseCase(inMemoryAuthorsRepository)
+  })
+
+  it('should be able to change an author password', async () => {
+    const author = makeAuthor()
+    const incriptedPassword = await hash(author.password, 6)
+    author.password = incriptedPassword
+
+    await inMemoryAuthorsRepository.create(author)
+
+    await sut.execute({
+      authorId: author.id,
+      newPassword: 'new-password',
+    })
+
+    const doesPasswordsMatchs = await compare(
+      'new-password',
+      inMemoryAuthorsRepository.authors[0].password,
+    )
+
+    expect(doesPasswordsMatchs).toBe(true)
+  })
+
+  it('should not be able to change password from an author with invalid ID', async () => {
+    await expect(() =>
+      sut.execute({
+        authorId: 'wrong ID',
+        newPassword: 'new-password',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+})
