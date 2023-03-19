14d5109ec4731cf563d05193ada9dba602cbaddb
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-19 14:27:23 -0300

    feat: create helper to handle token expiration time

---
+++ src/application/helpers/token-expiration.ts
@@ -0,0 +1,24 @@
+class Token {
+  private readonly SECONDS = 60
+  private readonly TOKEN_EXPIRE_IN_HOURS: number
+
+  constructor(hours: number) {
+    this.TOKEN_EXPIRE_IN_HOURS = this.SECONDS * this.SECONDS * hours
+  }
+
+  getTokenExpirationHours() {
+    return this.TOKEN_EXPIRE_IN_HOURS
+  }
+}
+
+export class AccessTokenExpiration extends Token {
+  constructor(hours = 1) {
+    super(hours)
+  }
+}
+
+export class RefreshTokenExpiration extends Token {
+  constructor(hours = 24) {
+    super(hours)
+  }
+}
