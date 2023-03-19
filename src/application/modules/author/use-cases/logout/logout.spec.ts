d6fe2c9d8888d090d827e3529f1af564a31cc145
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-19 18:43:28 -0300

    feat: logout use case specs

---
+++ src/application/modules/author/use-cases/logout/logout.spec.ts
@@ -0,0 +1,42 @@
+import { InMemoryAuthorsRepository } from '@/application/repositories/in-memory/in-memory-authors-repository'
+import { RedisMock } from '@/application/repositories/in-memory/redis-mock'
+import { hash } from 'bcryptjs'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { makeAuthor } from '../../factory/make-author'
+import { InvalidCredentialsError } from '../errors/invalid-credentials-error'
+import { LogoutUseCase } from './logout'
+
+let sut: LogoutUseCase
+let inMemoryAuthorsRepository: InMemoryAuthorsRepository
+let redisMock: RedisMock
+
+describe('Logout use case', () => {
+  beforeEach(() => {
+    inMemoryAuthorsRepository = new InMemoryAuthorsRepository()
+    redisMock = new RedisMock()
+    sut = new LogoutUseCase(inMemoryAuthorsRepository, redisMock)
+  })
+
+  it('should be able to disconnect an author', async () => {
+    const author = makeAuthor()
+    const incriptedPassword = await hash(author.password, 6)
+    author.password = incriptedPassword
+
+    await inMemoryAuthorsRepository.create(author)
+
+    await sut.execute({
+      authorId: author.id,
+    })
+
+    const authorToken = await redisMock.getValue(author.id)
+    expect(authorToken).toBeFalsy()
+  })
+
+  it('should not be able to disconnect a not found author', async () => {
+    await expect(() =>
+      sut.execute({
+        authorId: 'random-id',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+})
