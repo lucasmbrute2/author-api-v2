2d3c4e5282df66da97cd787fe3b87d7622a4847e
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-19 18:26:05 -0300

    feat: create change password use case

---
+++ src/application/modules/author/use-cases/change-password/change-password.ts
@@ -0,0 +1,25 @@
+import { AuthorsRepository } from '@/application/repositories/authors-repositories'
+import { hash } from 'bcryptjs'
+import { InvalidCredentialsError } from '../errors/invalid-credentials-error'
+
+interface ChangePasswordUseCaseProps {
+  authorId: string
+  newPassword: string
+}
+
+type ChangePasswordUseCaseResponse = void
+
+export class ChangePasswordUseCase {
+  constructor(private authorRepository: AuthorsRepository) {}
+
+  async execute({
+    authorId,
+    newPassword,
+  }: ChangePasswordUseCaseProps): Promise<ChangePasswordUseCaseResponse> {
+    const author = await this.authorRepository.findById(authorId)
+    if (!author) throw new InvalidCredentialsError()
+
+    author.password = await hash(newPassword, 6)
+    await this.authorRepository.save(author)
+  }
+}
