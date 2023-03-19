077b8ccff1cf4629a5717553a94ee446a8e7c678
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-19 12:35:38 -0300

    feat: add picture entity

---
+++ src/application/modules/picture/entities/picture.ts
@@ -0,0 +1,77 @@
+import { randomUUID } from 'node:crypto'
+
+interface PictureProps {
+  id?: string
+  htmlUrl: string
+  name: string
+  aliasKey: string
+  createdAt?: Date
+  deletedAt?: Date | null
+  authorId?: string
+}
+
+export class Picture {
+  constructor(private props: PictureProps) {
+    this.props = {
+      ...props,
+      id: this.props.id ?? randomUUID(),
+      createdAt: this.props.createdAt ?? new Date(),
+    }
+  }
+
+  get id(): string {
+    if (!this.props.id)
+      throw new Error('It was not possible to access the object ID')
+    return this.props.id
+  }
+
+  set htmlUrl(htmlUrl: string) {
+    this.props.htmlUrl = htmlUrl
+  }
+
+  get htmlUrl(): string {
+    return this.props.htmlUrl
+  }
+
+  set name(name: string) {
+    this.props.name = name
+  }
+
+  get name(): string {
+    return this.props.name
+  }
+
+  set aliasKey(aliasKey: string) {
+    this.props.aliasKey = aliasKey
+  }
+
+  get aliasKey(): string {
+    return this.props.aliasKey
+  }
+
+  get createdAt(): Date | undefined {
+    return this.props.createdAt
+  }
+
+  get deletedAt(): Date | undefined | null {
+    return this.props.deletedAt
+  }
+
+  delete() {
+    if (this.props.deletedAt) throw new Error('There is already deleted')
+    this.props.deletedAt = new Date()
+  }
+
+  unDoDelete() {
+    if (!this.props.deletedAt) throw new Error('This author is not deleted')
+    this.props.deletedAt = null
+  }
+
+  set authorId(authorId: string | undefined) {
+    this.props.authorId = authorId
+  }
+
+  get authorId(): string | undefined {
+    return this.props.authorId
+  }
+}
