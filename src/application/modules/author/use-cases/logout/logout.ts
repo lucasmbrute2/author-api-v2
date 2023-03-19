8dbc5d5b79b1499517ccbd01114ac96401388e12
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-19 18:43:37 -0300

    feat: logout use case

---
+++ src/application/modules/author/use-cases/logout/logout.ts
@@ -0,0 +1,21 @@
+import { AuthorsRepository } from '@/application/repositories/authors-repositories'
+import { RedisRepository } from '@/application/repositories/redis-repository'
+import { InvalidCredentialsError } from '../errors/invalid-credentials-error'
+
+interface LogoutUseCaseProps {
+  authorId: string
+}
+
+export class LogoutUseCase {
+  constructor(
+    private authorRepository: AuthorsRepository,
+    private redisClient: RedisRepository,
+  ) {}
+
+  async execute({ authorId }: LogoutUseCaseProps) {
+    const author = await this.authorRepository.findById(authorId)
+    if (!author) throw new InvalidCredentialsError()
+
+    await this.redisClient.delete(author.id)
+  }
+}
