76353ef4b265207f1b989f9eecb1b36a4a7e4fcb
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-20 18:53:32 -0300

    feat: add change password controller

---
+++ src/application/http/controllers/authors/change-password.ts
@@ -0,0 +1,27 @@
+import { ChangePasswordUseCase } from '@/application/modules/author/use-cases/change-password/change-password'
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
+interface ChangePasswordControllerProps {
+  newPassword: string
+  oldPassword: string
+}
+
+export class ChangePasswordController {
+  async handle(
+    req: Request<unknown, unknown, ChangePasswordControllerProps>,
+    res: Response,
+  ): Promise<Response> {
+    const { authorId } = req
+    const { newPassword, oldPassword } = req.body
+
+    const changePasswordAuthorUseCase = container.resolve(ChangePasswordUseCase)
+    await changePasswordAuthorUseCase.execute({
+      authorId,
+      newPassword,
+      oldPassword,
+    })
+
+    return res.status(204).send()
+  }
+}
