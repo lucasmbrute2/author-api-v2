e2331852ab0f8f040e3aafa422177c4e27033557
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-20 18:34:46 -0300

    feat: create middleare to validate if author is authenticated

--- src/application/http/controllers/authors/authenticate.ts
+++ src/application/http/controllers/authors/authenticate.ts
@@ -31,7 +31,7 @@ export class AuthenticateController {
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       sameSite: 'none',
-      maxAge: 24 * 60 * 60 * 1000,
+      maxAge: 1000 * 60 * 60 * 24, // 24h
       secure: true,
     })
 
---
+++ src/application/http/middlewares/ensure-authenticate.ts
@@ -0,0 +1,33 @@
+import { env } from '@/application/env'
+import { RedisProvider } from '@/application/repositories/redis/redis-provider'
+import { AppError } from '@/shared/errors/global-errors'
+import { NextFunction, Request, Response } from 'express'
+import { verify } from 'jsonwebtoken'
+import { container } from 'tsyringe'
+
+export class Authorization {
+  async ensureAuth(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers.authorization
+    const redisClient = container.resolve(RedisProvider)
+
+    try {
+      // @ts-ignore
+      // eslint-disable-next-line
+      const [_, token] = authHeader.split(' ')
+
+      const { sub: authorId } = verify(token, env.JWT_SECRET)
+
+      const isTokenAvailable = await redisClient.getValue(authorId as string)
+
+      req.authorId = authorId as string
+
+      if (!isTokenAvailable) {
+        throw new AppError('Missing token', 403)
+      }
+    } catch (error) {
+      throw new AppError('Missing token', 403)
+    }
+
+    return next()
+  }
+}
