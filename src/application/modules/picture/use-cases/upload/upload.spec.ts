a2107bd15b227f458871e43bdb493f611514fe33
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-20 19:57:05 -0300

    feat: upload picture specs & use case

---
+++ src/application/modules/picture/use-cases/upload/upload.spec.ts
@@ -0,0 +1,62 @@
+import 'reflect-metadata'
+import { InMemoryAuthorsRepository } from '@/application/repositories/in-memory/in-memory-authors-repository'
+import { InMemoryPictureRepository } from '@/application/repositories/in-memory/in-memory-picture-repository'
+import { NotFoundError } from '@/shared/errors/global-errors'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { makePicture } from '../../factory/make-picture'
+import { UploadUseCase } from './upload'
+import { makeAuthor } from '@/application/modules/author/factory/make-author'
+import { LocalStorageProvider } from '@/application/repositories/providers/storage/local-storage-provider'
+
+let storageProvider: LocalStorageProvider
+let inMemoryPicturesRepository: InMemoryPictureRepository
+let inMemoryAuthorsRepository: InMemoryAuthorsRepository
+let sut: UploadUseCase
+
+describe('Upload picture use case', () => {
+  beforeEach(() => {
+    inMemoryAuthorsRepository = new InMemoryAuthorsRepository()
+    inMemoryPicturesRepository = new InMemoryPictureRepository()
+    storageProvider = new LocalStorageProvider()
+
+    sut = new UploadUseCase(
+      storageProvider,
+      inMemoryAuthorsRepository,
+      inMemoryPicturesRepository,
+    )
+  })
+
+  it('should not be able to proceed with upload if author was not found', async () => {
+    const picture = makePicture()
+    await expect(() =>
+      sut.execute(picture, 'random-author-id'),
+    ).rejects.toBeInstanceOf(NotFoundError)
+  })
+
+  it('should be able to save a picture', async () => {
+    const author = makeAuthor()
+    await inMemoryAuthorsRepository.create(author)
+    const picture = makePicture()
+
+    await sut.execute(picture, author.id)
+
+    expect(inMemoryPicturesRepository.pictures).toHaveLength(1)
+    expect(inMemoryPicturesRepository.pictures).toEqual([
+      expect.objectContaining({
+        authorId: expect.any(String),
+        createdAt: expect.any(Date),
+      }),
+    ])
+  })
+
+  it('should be able to upload a picture', async () => {
+    const author = makeAuthor()
+    await inMemoryAuthorsRepository.create(author)
+    const picture = makePicture()
+
+    const { picture: createdPicture } = await sut.execute(picture, author.id)
+
+    expect(createdPicture).toHaveProperty('id')
+    expect(createdPicture).toHaveProperty('createdAt')
+  })
+})
---
+++ src/application/modules/picture/use-cases/upload/upload.ts
@@ -0,0 +1,46 @@
+import { AuthorsRepository } from '@/application/repositories/authors-repository'
+import { PictureRepository } from '@/application/repositories/pictures-repository'
+import { StorageRepository } from '@/application/repositories/storage-repository'
+import { NotFoundError } from '@/shared/errors/global-errors'
+import { inject, injectable } from 'tsyringe'
+import { Picture, PictureProps } from '../../entities/picture'
+
+interface UploadUseCaseResponse {
+  picture: Picture
+}
+
+@injectable()
+export class UploadUseCase {
+  constructor(
+    @inject('StorageProvider')
+    private storageProvider: StorageRepository,
+    @inject('AuthorRepository')
+    private authorRepository: AuthorsRepository,
+    @inject('PictureRepository')
+    private pictureRepository: PictureRepository,
+  ) {}
+
+  async execute(
+    file: PictureProps,
+    authorId: string,
+  ): Promise<UploadUseCaseResponse> {
+    const author = await this.authorRepository.findById(authorId)
+    if (!author) throw new NotFoundError('Author not found')
+
+    const picture = new Picture({
+      ...file,
+      authorId,
+    })
+
+    await this.pictureRepository.save(picture)
+    await this.storageProvider.save(picture)
+
+    // picture.htmlUrl = `https://${enviromentVariables.aws.bucketName}.s3.${enviromentVariables.aws.region}.amazonaws.com/test/${picture.aliasKey}`
+
+    // await this.pictureRepository.save(picture)
+
+    return {
+      picture,
+    }
+  }
+}
