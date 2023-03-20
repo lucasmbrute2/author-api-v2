a9e91bcf3d8fb47d3638673233ffd110d918f78c
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-20 18:35:22 -0300

    feat: add logout route

--- src/application/http/routes/author.routes.ts
+++ src/application/http/routes/author.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 import { AuthenticateController } from '../controllers/authors/authenticate'
+import { LogoutController } from '../controllers/authors/logout'
 import { RegisterController } from '../controllers/authors/register'
+import { Authorization } from '../middlewares/ensure-authenticate'
 import {
   validateAuthenticateSchema,
   validateRegisterSchema,
@@ -8,10 +10,16 @@ import {
 
 const authorRouter = Router()
 
+const authorization = new Authorization()
+
 const registerController = new RegisterController()
 const authController = new AuthenticateController()
+const logoutController = new LogoutController()
 
 authorRouter.post('/', validateRegisterSchema, registerController.handle)
 authorRouter.post('/session', validateAuthenticateSchema, authController.handle)
+
+// protected routes
+authorRouter.get('/logout', authorization.ensureAuth, logoutController.handle)
 
 export { authorRouter }
