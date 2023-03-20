08cc3a8b060370f131c09ec7361c74f835242b85
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-20 10:14:33 -0300

    feat: implement prisma author repository

--- src/application/repositories/prisma/prisma-authors-repository.ts
+++ src/application/repositories/prisma/prisma-authors-repository.ts
@@ -2,25 +2,48 @@ import { Author } from '@/application/modules/author/entities/author'
 import { inject } from 'tsyringe'
 import { AuthorsRepository } from '../authors-repositories'
 import { PrismaClient } from '@prisma/client'
+import { PrismaMapper } from './mappers/author-mapper'
 
 export class PrismaAuthorsRepository implements AuthorsRepository {
   private prisma: PrismaClient
 
-  constructor(@inject(PrismaClient) prisma: PrismaClient) {}
+  constructor(@inject(PrismaClient) prisma: PrismaClient) {
+    this.prisma = prisma
+  }
 
   async create(author: Author): Promise<void> {
-    throw new Error('Method not implemented.')
+    await this.prisma.author.create({
+      data: PrismaMapper.toPrisma(author),
+    })
   }
 
   async save(author: Author): Promise<void> {
-    throw new Error('Method not implemented.')
+    await this.prisma.author.update({
+      data: PrismaMapper.toPrisma(author),
+      where: {
+        id: author.id,
+      },
+    })
   }
 
   async findByUsername(username: string): Promise<Author | null> {
-    throw new Error('Method not implemented.')
+    const author = await this.prisma.author.findUnique({
+      where: {
+        username,
+      },
+    })
+
+    if (!author) return null
+    return PrismaMapper.toDomain(author)
   }
 
   async findById(authorId: string): Promise<Author | null> {
-    throw new Error('Method not implemented.')
+    const author = await this.prisma.author.findUnique({
+      where: {
+        id: authorId,
+      },
+    })
+    if (!author) return null
+    return PrismaMapper.toDomain(author)
   }
 }
