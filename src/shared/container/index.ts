943aeea364feb4c808fa08ee35ed6cc9fb29f315
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-20 09:59:08 -0300

    feat: add container injection with Redis and Prisma

---
+++ src/shared/container/index.ts
@@ -0,0 +1,10 @@
+import { RedisRepository } from '@/application/repositories/redis-repository'
+import { RedisProvider } from '@/application/repositories/redis/redis-provider'
+import { PrismaClient } from '@prisma/client'
+import { container } from 'tsyringe'
+
+container.register<PrismaClient>(PrismaClient, {
+  useValue: new PrismaClient(),
+})
+
+container.registerSingleton<RedisRepository>('RedisRepository', RedisProvider)
