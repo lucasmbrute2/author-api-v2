1b43e39a383e9c1e17dbb02f3de7f079364f09d5
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-20 14:58:48 -0300

    feat: middleware to validate body schema

---
+++ src/application/http/middlewares/validate-author-body-schema.ts
@@ -0,0 +1,26 @@
+import { AppError } from '@/shared/errors/global-errors'
+import { NextFunction, Request, Response } from 'express'
+import { z } from 'zod'
+
+export function validateBodySchema(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const validation = registerBodySchema.safeParse(req.body)
+
+  if (validation.success === false) {
+    throw new AppError(
+      `Invalid body: ${JSON.stringify(validation.error.format())}`,
+      400,
+    )
+  }
+
+  return next()
+}
--- src/application/http/routes/author.routes.ts
+++ src/application/http/routes/author.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { RegisterController } from '../controllers/authors/register'
+import { validateBodySchema } from '../middlewares/validate-author-body-schema'
 
 const authorRouter = Router()
 
 const registerController = new RegisterController()
 
-authorRouter.post('/', registerController.handle)
+authorRouter.post('/', validateBodySchema, registerController.handle)
 
 export { authorRouter }
