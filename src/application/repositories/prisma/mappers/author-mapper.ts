04f3ffc0894b217032f427603146e5390bc8d258
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-20 10:14:43 -0300

    feat: add toDomain method to prisma mapper

--- src/application/repositories/prisma/mappers/author-mapper.ts
+++ src/application/repositories/prisma/mappers/author-mapper.ts
@@ -2,7 +2,7 @@ import { Author } from '@/application/modules/author/entities/author'
 import { Author as rawAuthor } from '@prisma/client'
 
 export class PrismaMapper {
-  static ToPrisma(author: Author): rawAuthor {
+  static toPrisma(author: Author): rawAuthor {
     const {
       bio,
       id,
@@ -24,5 +24,29 @@ export class PrismaMapper {
       refreshToken: refreshToken ?? null,
       username,
     }
+  }
+
+  static toDomain(rawAuthor: rawAuthor): Author {
+    const {
+      bio,
+      created_at,
+      deleted_at,
+      id,
+      name,
+      password,
+      refreshToken,
+      username,
+    } = rawAuthor
+
+    return new Author({
+      bio: bio ?? undefined,
+      name,
+      password,
+      id,
+      refreshToken: refreshToken ?? undefined,
+      username,
+      createdAt: created_at,
+      deletedAt: deleted_at,
+    })
   }
 }
