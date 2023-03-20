5fb941facd2d40eaa0e0159081b70cea745488a8
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-20 15:46:21 -0300

    feat: session route

--- src/application/http/routes/author.routes.ts
+++ src/application/http/routes/author.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express'
+import { AuthenticateController } from '../controllers/authors/authenticate'
 import { RegisterController } from '../controllers/authors/register'
-import { validateBodySchema } from '../middlewares/validate-author-body-schema'
+import {
+  validateAuthenticateSchema,
+  validateRegisterSchema,
+} from '../middlewares/validate-author-body-schema'
 
 const authorRouter = Router()
 
 const registerController = new RegisterController()
+const authController = new AuthenticateController()
 
-authorRouter.post('/', validateBodySchema, registerController.handle)
+authorRouter.post('/', validateRegisterSchema, registerController.handle)
+authorRouter.post('/session', validateAuthenticateSchema, authController.handle)
 
 export { authorRouter }
