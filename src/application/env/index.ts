a8ee3d693ef4baaf33f2bb8025ce430d9d65c77b
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-20 09:47:06 -0300

    feat: validating redis variable environments

--- src/application/env/index.ts
+++ src/application/env/index.ts
@@ -4,6 +4,10 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   JWT_SECRET: z.string(),
+  REDISPORT: z.coerce.number().default(5432),
+  REDISPASSWORD: z.string(),
+  REDISUSER: z.string(),
+  REDISHOST: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
--- src/application/repositories/redis-repository.ts
+++ src/application/repositories/redis-repository.ts
@@ -1,5 +1,5 @@
 export interface RedisRepository {
   setValue(key: string, value: string, expire: number): Promise<void>
-  getValue(key: string): Promise<string>
+  getValue(key: string): Promise<string | null>
   delete(key: string): Promise<void>
 }
