42197e6a0886e6e34c7f30909a5bd50ce2539251
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-20 21:02:44 -0300

    feat: add delete picture use case & spec

---
+++ src/application/modules/picture/use-cases/delete/delete.ts
@@ -0,0 +1,27 @@
+import { AuthorsRepository } from '@/application/repositories/authors-repository'
+import { PictureRepository } from '@/application/repositories/pictures-repository'
+import { StorageRepository } from '@/application/repositories/storage-repository'
+import { NotFoundError } from '@/shared/errors/global-errors'
+import { inject, injectable } from 'tsyringe'
+
+type DeletePictureUseCaseResponse = void
+
+@injectable()
+export class DeletePictureUseCase {
+  constructor(
+    @inject('StorageProvider')
+    private storageProvider: StorageRepository,
+    @inject('AuthorRepository')
+    private authorRepository: AuthorsRepository,
+    @inject('PictureRepository')
+    private pictureRepository: PictureRepository,
+  ) {}
+
+  async execute(aliasKey: string): Promise<DeletePictureUseCaseResponse> {
+    const picture = await this.pictureRepository.findByAliasKey(aliasKey)
+    if (!picture) throw new NotFoundError('Picture not found')
+
+    await this.pictureRepository.delete(aliasKey)
+    // await this.storageProvider.delete(aliasKey)
+  }
+}
