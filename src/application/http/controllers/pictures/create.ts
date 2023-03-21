b5723c71dc108b196630e4c7eb26c5fef472d40e
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-20 22:59:24 -0300

    feat: add create picture controller

---
+++ src/application/http/controllers/pictures/create.ts
@@ -0,0 +1,34 @@
+import { CreatePictureUseCase } from '@/application/modules/picture/use-cases/create/create'
+import { PictureViewModel } from '@/application/views/picture-view-model'
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+import { z } from 'zod'
+
+export class CreatePictureController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const createPictureBoddySchema = z.object({
+      path: z.string(),
+      originalname: z.string(),
+      filename: z.string(),
+      size: z.coerce.number(),
+      mimetype: z.string(),
+    })
+
+    const { filename, originalname, path } = createPictureBoddySchema.parse(
+      req.file,
+    )
+    const { authorId } = req
+
+    const createPictureUseCase = container.resolve(CreatePictureUseCase)
+    const { picture } = await createPictureUseCase.execute({
+      aliasKey: filename,
+      htmlUrl: path,
+      name: originalname,
+      authorId,
+    })
+
+    return res.status(201).json({
+      picture: PictureViewModel.toHTTP(picture),
+    })
+  }
+}
