5a61b58cf743306c044e6303195b711fa4540d57
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-21 13:28:10 -0300

    feat: add refresh token controller

---
+++ src/application/http/controllers/authors/refresh.ts
@@ -0,0 +1,31 @@
+import { RefreshTokenUseCase } from '@/application/modules/author/use-cases/refresh-token/refresh'
+import { AuthorViewModel } from '@/application/views/author-view-model'
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
+export class RefreshTokenController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { refreshToken } = req.cookies
+
+    const refreshTokenAuthorUseCase = container.resolve(RefreshTokenUseCase)
+    const {
+      accessToken,
+      author,
+      refreshToken: newRefreshToken,
+    } = await refreshTokenAuthorUseCase.execute({
+      refreshToken,
+    })
+
+    res.cookie('refreshToken', newRefreshToken, {
+      httpOnly: true,
+      sameSite: 'none',
+      maxAge: 60 * 60 * 1000, // 24h
+      secure: true,
+    })
+
+    return res.status(200).json({
+      author: AuthorViewModel.toHTTP(author),
+      accessToken,
+    })
+  }
+}
