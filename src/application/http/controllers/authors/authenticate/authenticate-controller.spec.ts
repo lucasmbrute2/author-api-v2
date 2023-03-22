55a49feced2c9f784bba3f81ba19aed36fc96890
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-21 23:52:06 -0300

    feat: add authenticate controller e2e spec

---
+++ src/application/http/controllers/authors/authenticate/authenticate-controller.spec.ts
@@ -0,0 +1,24 @@
+import request from 'supertest'
+import { app } from '@/application/app'
+import { describe, expect, it } from 'vitest'
+
+describe('Authenticate (e2e)', () => {
+  it('should be able to authenticate', async () => {
+    await request(app).post('/author').send({
+      name: 'John Doe',
+      username: 'johndoe@gmail.com',
+      password: 'password123',
+      confirmPassword: 'password123',
+    })
+
+    const response = await request(app).patch('/author/session').send({
+      username: 'johndoe@gmail.com',
+      password: 'password123',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      accessToken: expect.any(String),
+    })
+  })
+})
