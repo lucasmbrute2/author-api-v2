82a75457bf68692cabfaf77531343c0e761f4d02
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-21 23:07:50 -0300

    chore: setup prisma and redis to e2e databases

--- tests/vitest-environment-e2e-databases/e2e-databases.ts
+++ tests/vitest-environment-e2e-databases/e2e-databases.ts
@@ -1,13 +1,46 @@
+import 'dotenv/config'
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
+import { execSync } from 'node:child_process'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generatePrismaDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+  url.searchParams.set('schema', schema)
+  return url.toString()
+}
+
+function generateRedisDatabase() {
+  if (!process.env.REDISPASSWORD || !process.env.REDISUSER) {
+    throw new Error('Please provide a REDIS environment variables')
+  }
+
+  process.env.REDISPASSWORD = randomUUID()
+  process.env.REDISUSER = randomUUID()
+}
 
 export default <Environment>{
-  name: 'e2e-databases', // nome no enviroment
+  name: 'e2e-databases',
   async setup() {
-    console.log('setup')
+    const prismaSchema = randomUUID()
+    const databaseURL = generatePrismaDatabaseUrl(prismaSchema)
+    process.env.DATABASE_URL = databaseURL
+    execSync('npx prisma migrate deploy')
+
+    generateRedisDatabase()
 
     return {
       async teardown() {
-        console.log('teardown')
+        await prisma.$queryRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${prismaSchema}" CASCADE`,
+        )
+        await prisma.$disconnect()
       },
     }
   },
