3096daa3d99f92e3e2409d321fa7e4f8bce9f964
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-21 23:51:56 -0300

    feat: add register controller e2e spec

--- src/application/http/controllers/authors/register/register-controller.spec.ts
+++ src/application/http/controllers/authors/register/register-controller.spec.ts
@@ -1,5 +1,21 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/application/app'
+import { describe, expect, it } from 'vitest'
 
-test('ok', () => {
-  console.log('i,m here')
+describe('Register (e2e)', () => {
+  it('should be able to register', async () => {
+    const response = await request(app).post('/author').send({
+      name: 'John Doe',
+      username: 'johndoe@gmail.com',
+      password: 'password123',
+      confirmPassword: 'password123',
+    })
+
+    expect(response.statusCode).toEqual(201)
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        accessToken: expect.any(String),
+      }),
+    )
+  })
 })
