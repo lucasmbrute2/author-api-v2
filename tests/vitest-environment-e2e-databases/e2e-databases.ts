0389df956e993cfcd2ce2f7fa71f0dd3af095baf
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-22 00:13:23 -0300

    chore: remove redis

--- tests/vitest-environment-e2e-databases/e2e-databases.ts
+++ tests/vitest-environment-e2e-databases/e2e-databases.ts
@@ -3,7 +3,6 @@ import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
 import { execSync } from 'node:child_process'
 import { PrismaClient } from '@prisma/client'
-import { Redis } from 'ioredis'
 
 const prisma = new PrismaClient()
 
@@ -17,22 +16,6 @@ function generatePrismaDatabaseUrl(schema: string) {
   return url.toString()
 }
 
-function generateRedisDatabase(): Redis {
-  if (!process.env.REDISPASSWORD || !process.env.REDISUSER) {
-    throw new Error('Please provide a REDIS environment variables')
-  }
-
-  // TODO mock Redis RB instead 'real' db
-  const redis = new Redis({
-    port: 6379,
-    host: 'localhost',
-    username: process.env.REDISUSER,
-    password: process.env.REDISPASSWORD,
-  })
-
-  return redis
-}
-
 export default <Environment>{
   name: 'e2e-databases',
   async setup() {
@@ -41,16 +24,12 @@ export default <Environment>{
     process.env.DATABASE_URL = databaseURL
     execSync('npx prisma migrate deploy')
 
-    generateRedisDatabase()
-    const redis = generateRedisDatabase()
-
     return {
       async teardown() {
         await prisma.$queryRawUnsafe(
           `DROP SCHEMA IF EXISTS "${prismaSchema}" CASCADE`,
         )
         await prisma.$disconnect()
-        redis.disconnect()
       },
     }
   },
